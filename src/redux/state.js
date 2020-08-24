4cce42c613f076b8327afe20e4afb3345648ec0c
iniken <kiparis994@gmail.com>
2020-08-24 20:21:53 +0700

    35) Add callback, subscribe, observer

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,4 +1,6 @@
-import {rerenderEntireTree} from "../render";
+let rerenderEntireTree = () => {
+    console.log("State changed")
+}
 
 let state = {
     profilePage: {
@@ -31,7 +33,9 @@ let state = {
     }
 }
 
-export let addPost = () => {
+window.state = state;
+
+export const addPost = () => {
     let  newPost = {
         id: 5,
         message: state.profilePage.newPostText,
@@ -42,9 +46,13 @@ export let addPost = () => {
     rerenderEntireTree(state);
 }
 
-export let updateNewPostText = (newText) => {
+export const updateNewPostText = (newText) => {
     state.profilePage.newPostText = newText;
     rerenderEntireTree(state);
+}
+
+export const subscribe = (observer) => {
+    rerenderEntireTree = observer;
 }
 
 export default state;
