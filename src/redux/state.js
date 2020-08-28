d850fea4438340440dfe20155209cc3086f7e9ef
iniken <kiparis994@gmail.com>
2020-08-28 20:00:56 +0300

    38) UPDATE-NEW-POST-TEXT

--- src/redux/state.js
+++ src/redux/state.js
@@ -1,5 +1,3 @@
-
-
 let store = {
     _state: {
         profilePage: {
@@ -13,7 +11,7 @@ let store = {
             newPostText: "it-kamasutra.com"
         },
         dialogsPage: {
-            dialogs : [
+            dialogs: [
                 {id: "1", name: "Dimych"},
                 {id: "2", name: "Andrey"},
                 {id: "3", name: "Sveta"},
@@ -21,7 +19,7 @@ let store = {
                 {id: "5", name: "Viktor"},
                 {id: "6", name: "Valera"}
             ],
-            messages : [
+            messages: [
                 {id: "1", message: "Hi"},
                 {id: "2", message: "How is you it-kamasutra?"},
                 {id: "3", message: "How are you?"},
@@ -38,12 +36,12 @@ let store = {
     getState() {
         return this._state;
     },
-    subscribe(observer)  {
+    subscribe(observer) {
         this._callSubscriber = observer;
     },
 
     addPost() {
-        let  newPost = {
+        let newPost = {
             id: 5,
             message: this._state.profilePage.newPostText,
             likesCount: 0
@@ -57,16 +55,19 @@ let store = {
         this._callSubscriber(this._state);
     },
     dispatch(action) {
-if(action.type ==="ADD-POST") {
-    let  newPost = {
-        id: 5,
-        message: this._state.profilePage.newPostText,
-        likesCount: 0
-    };
-    this._state.profilePage.posts.push(newPost);
-    this._state.profilePage.newPostText = " ";
-    this._callSubscriber(this._state);
-}
+        if (action.type === "ADD-POST") {
+            let newPost = {
+                id: 5,
+                message: this._state.profilePage.newPostText,
+                likesCount: 0
+            };
+            this._state.profilePage.posts.push(newPost);
+            this._state.profilePage.newPostText = " ";
+            this._callSubscriber(this._state);
+        } else if (action.type === "UPDATE-NEW-POST-TEXT") {
+            this._state.profilePage.newPostText = newText;
+            this._callSubscriber(this._state);
+        }
     }
 }
 
